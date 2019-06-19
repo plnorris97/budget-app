3bdb89bd306c2fdb9f0df967ff6c8fea35f5861b
Tricia Norris <tricianorris97@gmail.com>
2019-06-19 15:34:15 -0400

    built the two form submit functions; updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # budget-app
+
+## Description
+This project was created from the FreeCodeCamp tutorial, Javascript Project Tutorial: Budget App. The video is found here: https://www.youtube.com/watch?time_continue=1012&v=m_HJ3juuFvo. Using classes and a constructor, I am building the functionality for the budget calculations.
+
--- js/app.js
+++ js/app.js
@@ -43,18 +43,94 @@ class UI {
   // submit budget method
   submitBudgetForm(){
       const value = this.budgetInput.value;
-      if(value===''||value<0){
+      if(value===''||value<0) {
           this.budgetFeedback.classList.add('showItem');
           this.budgetFeedback.innerHTML = `<p>Cannot be empty or negative.</p>`;
           const self = this;
 
-          setTimeout(function(){
+          setTimeout(function() {
               self.budgetFeedback.classList.remove('showItem')
               
           }, 4000);
-      }
+      } else {
+         this.budgetAmount.textContent = value;
+         this.budgetInput = '';
+         this.showBalance();   
+    }
+  }
+  // show balance
+  showBalance(){
+     const expense = this.totalExpense();
+     const total = parseInt(this.budgetAmount.textContent) - expense;
+     this.balanceAmount.textContent = total
+     if(total < 0) {
+       this.balance.classList.remove('showGreen', 'showBlack');
+       this.balance.classList.add('showRed');
+     } else if(total > 0) {
+       this.balance.classList.remove('showRed', 'showBlack');
+       this.balance.classList.add('showGreen');
+     } else if(total === 0) {
+       this.balance.classList.remove('showRed', 'showGreen');
+       this.balance.classList.add('showBlack');
+    }
+  }
+  // submit expense
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0) {
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>Values cannot be empty or negative.</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.expenseFeedback.classList.remove('showItem');
+      }, 4000);
+    } else {
+       let amount = parseInt(amountValue);
+       this.expenseInput.value = "";
+       this.amountInput.value = "";
+
+       let expense = {
+         id: this.itemID,
+         title: expenseValue,
+         amount: amount
+       };
+       this.itemID++;
+       this.itemList.push(expense);
+       this.addExpense(expense);
+    }
   }
 
+  //add expense
+
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
+  // total expense
+  totalExpense() {
+    let total = 400;
+    return total;
+  }
 }
 
 // the three variables that will hold activity on the page
@@ -74,6 +150,7 @@ function eventListeners(){
     // expense form submit
     expenseForm.addEventListener('submit', function(event){
         event.preventDefault();
+        ui.submitExpenseForm();
     })
     // expense list submit
     expenseList.addEventListener('click', function(){
